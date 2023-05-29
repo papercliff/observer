43b5fb706937ca08163dfc28be8893efb7e10334
Dimos Michailidis <dimos86m@gmail.com>
2023-05-29 23:24:52 +0300

    Not news-related only redirect URLs

--- src/observer/text.clj
+++ src/observer/text.clj
@@ -52,7 +52,7 @@
       (let [key-words (s/join " · " clique)
             link (str "https://papercliff.github.io/redirect/?q="
                       (s/join "+" clique)
-                      "&tbm=nws&tbs=cdr:1,cd_min:"
+                      "&tbs=cdr:1,cd_min:"
                       (-> now dt/at-start-of-prev-day dt/->us-day-str)
                       ",cd_max:"
                       (-> now dt/at-start-of-next-day dt/->us-day-str))
