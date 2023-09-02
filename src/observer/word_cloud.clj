30893b8c4b7270caa0a80e5844f73170eef78896
dimos <dimos86m@gmail.com>
2023-09-02 22:52:35 +0300

    Delete personal-like words

--- src/observer/word_cloud.clj
+++ src/observer/word_cloud.clj
@@ -42,14 +42,14 @@
   (* 30 (- 0.5 (rand))))
 
 (defn- stop-words []
-  (->> ["i" "me" "my" "myself" "we" "our" "ours" "ourselves" "you" "your" "yours" "yourself" "yourselves" "he" "him"
-        "his" "himself" "she" "her" "hers" "herself" "it" "its" "itself" "they" "them" "their" "theirs" "themselves"
-        "what" "which" "who" "whom" "this" "that" "these" "those" "am" "is" "are" "was" "were" "be" "been" "being"
-        "have" "has" "had" "having" "do" "does" "did" "doing" "a" "an" "the" "and" "but" "if" "or" "because" "as"
-        "until" "while" "of" "at" "by" "for" "with" "about" "against" "between" "into" "through" "during" "before"
-        "after" "above" "below" "to" "from" "up" "down" "in" "out" "on" "off" "over" "under" "again" "further" "then"
-        "once" "here" "there" "when" "where" "why" "how" "all" "any" "both" "each" "few" "more" "most" "other" "some"
-        "such" "no" "nor" "not" "only" "own" "same" "so" "than" "too" "very" "can" "will" "just" "don" "should" "now"]
+  (->> ["he" "him" "his" "himself" "she" "her" "hers" "herself" "it" "its" "itself" "they" "them" "their" "theirs"
+        "themselves" "what" "which" "who" "whom" "this" "that" "these" "those" "is" "are" "was" "were" "be" "been"
+        "being" "have" "has" "had" "having" "do" "does" "did" "doing" "a" "an" "the" "and" "but" "if" "or" "because"
+        "as" "until" "while" "of" "at" "by" "for" "with" "about" "against" "between" "into" "through" "during"
+        "before" "after" "above" "below" "to" "from" "up" "down" "in" "out" "on" "off" "over" "under" "again"
+        "further" "then" "once" "here" "there" "when" "where" "why" "how" "all" "any" "both" "each" "few" "more"
+        "most" "other" "some" "such" "no" "nor" "not" "only" "own" "same" "so" "than" "too" "very" "can" "will"
+        "just" "don" "should" "now"]
        (repeat 5)
        (apply concat)
        shuffle
