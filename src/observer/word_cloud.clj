2db07f9223f36f7a5db6afe7385697c2580a1fcb
dimos <dimos86m@gmail.com>
2023-09-02 23:18:57 +0300

    Colorless

--- src/observer/word_cloud.clj
+++ src/observer/word_cloud.clj
@@ -14,29 +14,16 @@
 (def height 607)
 
 (def background-color
-  (Color. 255 255 255))
+  (Color. 0 0 0))
 
 (def color-palette
-  (map
-    #(Color. %)
-    [0x97C2FC
-     0xFB7E81
-     0x7BE141
-     0xEB7DF4
-     0xAD85E4
-     0xFFA807
-     0x6E6EFD
-     0xFFC0CB
-     0xC2FABC
-     0xEE0000
-     0xFF6000
-     0x2B7CE9
-     0x255C03
-     0xFF007E
-     0x7C29F0
-     0xFD5A77
-     0x74D66A
-     0x990000]))
+  (->> (range 51 256 5)
+       (map
+         #(Color.
+            ^Integer %
+            ^Integer %
+            ^Integer %))
+       shuffle))
 
 (defn- rand-offset []
   (* 30 (- 0.5 (rand))))
@@ -54,8 +41,7 @@
        (apply concat)
        shuffle
        (map
-         #(WordFrequency.
-            % (inc (rand-int 2))))))
+         #(WordFrequency. % 1))))
 
 (defn- word-frequencies [selected-context-keywords]
   (concat
@@ -88,12 +74,12 @@
     (.fillOval g width-pad height-pad init-width-oval init-height-oval)
     (doseq [_ (range 1 number-of-circles)]
       (let [t (* 2 Math/PI (rand))
-            x-center (+ h (* a (Math/cos t)))
-            y-center (+ k (* b (Math/sin t)))
+            x-center (+ h (* a (Math/cos t)) (rand-offset))
+            y-center (+ k (* b (Math/sin t)) (rand-offset))
             small-oval-width (+ (* width small-oval-ratio) (rand-offset))
             small-oval-height (+ (* height small-oval-ratio) (rand-offset))
-            x (- x-center (/ small-oval-width 2) (rand-offset))
-            y (- y-center (/ small-oval-height 2) (rand-offset))]
+            x (- x-center (/ small-oval-width 2))
+            y (- y-center (/ small-oval-height 2))]
         (.fillOval g x y small-oval-width small-oval-height)))
     (.dispose g)
     (ImageIO/write image "PNG" (File. ^String output-path))))
@@ -107,7 +93,7 @@
     (.setBackgroundColor word-cloud background-color)
     (.setColorPalette word-cloud (ColorPalette. color-palette))
     (.setFontScalar word-cloud (LinearFontScalar. 5 50))
-    (.setAngleGenerator word-cloud (AngleGenerator. -10 10 10))
+    (.setAngleGenerator word-cloud (AngleGenerator. -20 20 20))
     (.build word-cloud (word-frequencies selected-context-keywords))
     (.writeToFile word-cloud output-path)))
 
